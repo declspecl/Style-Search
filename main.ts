c8f14347f6b8783f9f79218dce8036a9ba633ac6
dec-lied <gavindhondt@hotmail.com>
2023-03-20 15:22:50 -0400

    initial commit

--- main.ts
+++ main.ts
@@ -1,137 +1,178 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownPostProcessorContext, MarkdownRenderChild, MarkdownRenderer, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, ToggleComponent } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface MyPluginSettings
+{
+	active: boolean;
+	pattern_styling: Map<string, string>;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: MyPluginSettings =
+{
+	active: true,
+	pattern_styling: new Map<string, string>()
 }
 
-export default class MyPlugin extends Plugin {
+export default class SearchStyle extends Plugin
+{
 	settings: MyPluginSettings;
 
-	async onload() {
+	async onload()
+	{
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addCommand(
+		{
+			id: 'search-style-toggle',
+			name: 'Toggle Search Style Highlighting',
+			callback: async () =>
+			{
+				this.settings.active = !this.settings.active;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.saveSettings();
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SearchStyleSettings(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) =>
+		{
 			console.log('click', evt);
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerMarkdownPostProcessor((element, context) =>
+		{
+			const search_criteria: RegExp = /236P/g;
+			const valid_elements = element.querySelectorAll(":not(script)") as NodeListOf<HTMLElement>;
+
+			console.log("postprocessor called: " + valid_elements);
+
+			for (let i = 0; i < valid_elements.length; i++)
+			{
+				if (search_criteria.test(valid_elements[i].textContent as string))
+				{
+					context.addChild(new SearchStyleMatch(valid_elements.item(i), "236P"));
+				}
+			}
+		})
+
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	onunload()
+	{
+		this.saveSettings();
 	}
 
-	async loadSettings() {
+	async loadSettings()
+	{
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
-	async saveSettings() {
+	async saveSettings()
+	{
 		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class SearchStyleMatch extends MarkdownRenderChild
+{
+	text: string;
+
+	constructor(containerEl: HTMLElement, text: string)
+	{
+		super(containerEl);
+
+		this.text = text;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	onload()
+	{
+		const matchEl = this.containerEl.createSpan(
+		{
+		  	text: this.text
+		});
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		this.containerEl.replaceWith(matchEl);
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SearchStyleSettings extends PluginSettingTab
+{
+	plugin: SearchStyle;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SearchStyle)
+	{
 		super(app, plugin);
+
 		this.plugin = plugin;
 	}
 
-	display(): void {
+	display(): void
+	{
+		console.table(this.plugin.settings);
+
 		const {containerEl} = this;
 
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: "Search Style Settings"});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Display highlighting')
+			.setDesc('Controls whether the highlighting is active or not')
+			.addToggle(toggleElement => toggleElement
+				.onChange(new_value =>
+				{
+					this.plugin.settings.active = new_value;
+
+					this.plugin.saveSettings();
+				})
+				.setValue(this.plugin.settings.active))
+
+		new Setting(containerEl)
+			.setName("RegExp Search Criteria")
+			.setDesc("Regular expression patterns to search markdown against (separate by newlines)")
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+				.setPlaceholder("^Hello, World!$")
+				.inputEl.id = "pattern_input")
+			.addButton(button => button
+				.setButtonText("Add")
+				.onClick(evt =>
+				{
+					const pattern_input: HTMLInputElement = this.containerEl.querySelector("#pattern_input") as HTMLInputElement;
+					
+					this.plugin.settings.pattern_styling = this.plugin.settings.pattern_styling.set(pattern_input.value, "");
+
+					pattern_input.value = "";
+
+					this.plugin.saveSettings();
+					this.display();
+				}))
+
+		for (const pattern of this.plugin.settings.pattern_styling.keys())
+		{
+			new Setting(containerEl)
+				.setName(pattern + " Styling")
+				.setDesc("The CSS style that will be applied to " + pattern)
+				.addText(text => text
+					.setPlaceholder("background-color: lime;\ncolor: red;")
+					.onChange(new_value =>
+					{
+						this.plugin.settings.pattern_styling.set(pattern, new_value);
+
+						this.plugin.saveSettings();
+					}))
+				.addButton(button => button
+					.setIcon("trash-2")
+					.onClick(evt =>
+					{
+						this.plugin.settings.pattern_styling.delete(pattern);
+
+						this.plugin.saveSettings();
+						this.display();
+					}))
+		}
+
+		console.log("pattern_styling: ");
+		console.table(this.plugin.settings.pattern_styling);
 	}
 }
